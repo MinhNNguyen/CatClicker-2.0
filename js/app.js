8b6c1c855c8df0124584b717e8dcac9738aa3b8f
Minh Nguyen <thanhnhatminhnguyen@gmail.com>
2018-08-13 20:07:25 -0700

    Seperating ViewModel and Model

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/golden.jpg');
 	this.imgAttribution = ko.observable('https://www.google.com/');
-	this.nicknames = ko.observableArray([
-		{nickname: "Teddy Bear"},
-		{nickname: "Meo khong phai cho"},
-		{nickname: "Only know eating"}])
-
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
-
+	this.nicknames = ko.observableArray([ "Teddy Bear", "Meo khong phai cho", "Only know eating" ])
 	this.level = ko.computed(function() {
 		var clicks = this.clickCount();
 		if ( clicks <= 4) {
@@ -22,6 +14,16 @@ var ViewModel = function() {
 			return "Senior";
 		}
 	}, this);
+}
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat() );
+
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+
 }
 
 ko.applyBindings( new ViewModel() );
